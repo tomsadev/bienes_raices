ee9ba8f2ecf366f9ac8c6400c03c383f9c4add4a
tomsadev <tomsaezdev23@gmail.com>
2022-07-27 23:47:24 -0500

    Creando paginas para el administrador de la Web, crear, borrar y Actualizar propiedades

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,12 +1,63 @@
 <?php 
 
+    //Base de Datos //
+    
+    require '../../includes/config/database.php';
+    $db = conectarDB();
+
+
     require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
 
 
     <main class="contenedor">
-        <h1>Titulo Pagina</h1>
+        <h1>Crear</h1>
+
+        <a href="/admin/" class="boton boton-verde">Volver</a>
+
+        <form class="formulario">
+        <fieldset>
+            <legend>Informacion General</legend>
+
+            <label for="titulo">Titulo:</label>
+            <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+            <label for="precio">Precio:</label>
+            <input type="number" id="precio" placeholder="Precio de la Propiedad">
+
+            <label for="imagen">Imagen:</label>
+            <input type="file" id="imagen" accept="image/jpeg, imagen/png">
+
+            <label for="descripcion">Descripcion</label>
+            <textarea id="descripcion"></textarea>
+        </fieldset>
+
+        <fieldset>
+            <legend>Informacion de Propiedad</legend>
+
+            <label for="habitaciones">Habitaciones:</label>
+            <input type="number" id="habitaciones" placeholder="Ejmp: 3" min="1" max="9">
+
+            <label for="wc">Baños:</label>
+            <input type="number" id="wc" placeholder="Ejmp: 3" min="1" max="9">
+
+            <label for="estacionamiento">Estacionamientos:</label>
+            <input type="number" id="estacionamiento" placeholder="Ejmp: 3" min="1" max="9">
+        </fieldset>
+
+        <fieldset>
+            <legend>Vendedor</legend>
+
+            <select>
+                <option value="1">Tomas</option>
+                <option value="1">Karen</option>
+            </select>
+        </fieldset>
+
+        <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+
+    </form>
     </main>
 
 
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -13,15 +13,15 @@
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/">
-                    <img src="build/img/logo.svg" alt="logo">
+                    <img src="/build/img/logo.svg" alt="logo">
                 </a>
 
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu responsive">
+                    <img src="/build/img/barras.svg" alt="icono menu responsive">
                 </div>
 
                 <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="boton dark mode">
+                    <img class="dark-mode-boton" src="/build/img/dark-mode.svg" alt="boton dark mode">
                     <nav class="navegacion">
                         <a href="nosotros.php">Nosotros</a>
                         <a href="anuncios.php">Anuncios</a>
